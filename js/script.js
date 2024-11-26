792bd33b640f4f81046f1981e2f480d951ed4d5d
Julia <julyreis.correa@gmail.com>
2024-11-25 21:51:58 -0300

    Coisas Aula18

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function mostraAlerta() {
+    alert("Isso funciona mesmo!")
+}
+
+function mostraAlerta1(msg) {
+    alert("  Ooii Monoite " + msg)
+}
+
+function soma(n1, n2) {
+    n1= parseFloat (n1);
+    n2= parseFloat (n2);
+    soma= n1+n2;
+    return(soma);
+} 
+
+function dobro() {
+    var valor1, result
+    valor1 = document.getElementById("idN")
+    result = parseFloat(valor1.value)
+    +parseFloat(valor1.value);
+    document.getElementById("idResult").innerHTML = result;
+}
+
+function calculadora(op) {
+    var valor1, valor2, result
+    valor1= document.getElementById ("idN11")
+    valor2= document.getElementById ("idN22")
+
+    if(op == "+") {
+        result = parseFloat(valor1.value)+parseFloat(valor2.value);
+    }
+    document.getElementById("idCasa").
+        value = result;
+        console.log(result) 
+}
